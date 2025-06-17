a3bfc8d8e09c48cd88bcfbc48fc08dcc215e7b81
Rafael_Tomokame <rafaelshinjitomokame@gmail.com>
2025-06-17 18:51:19 -0300

    Delete atividade8 directory

--- atividade8/front/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-// src/services/api.ts
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3001', // Altere esta URL se o seu backend estiver em outro endereço/porta
-});
-
-export default api;
